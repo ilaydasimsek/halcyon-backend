6a33c83aea9e689d48fb454dddd7fb7a74716dbc
Onur Ari <onur@hellojasper.com>
2023-08-22 11:28:39 +0300

    Add yoga practice detail query

--- yoga_practices/graphql/queries.py
+++ yoga_practices/graphql/queries.py
@@ -2,13 +2,14 @@ import graphene
 from graphene import ConnectionField
 from graphql_jwt.decorators import login_required
 
-from yoga_practices.graphql.types import YogaPracticeConnection, YogaChallengeConnection
+from yoga_practices.graphql.types import YogaPracticeConnection, YogaChallengeConnection, YogaPracticeNode
 from yoga_practices.models import YogaPractice, YogaChallenge
 
 
 class Query(graphene.ObjectType):
     yoga_practices = ConnectionField(YogaPracticeConnection)
     yoga_challenges = ConnectionField(YogaChallengeConnection)
+    yoga_practice = graphene.Field(YogaPracticeNode, id=graphene.Int(required=True))
 
     @login_required
     def resolve_yoga_practices(self, info, **kwargs):
@@ -17,3 +18,7 @@ class Query(graphene.ObjectType):
     @login_required
     def resolve_yoga_challenges(self, info, **kwargs):
         return YogaChallenge.objects.all()
+
+    @login_required
+    def resolve_yoga_practice(self, info, id):
+        return YogaPractice.objects.get(id=id)
