75d5a74fac9d07cd590cef41eed8218a18a91e31
Onur Ari <onur@hellojasper.com>
2023-09-04 14:40:27 +0300

    Create yoga journey lesson models

--- yoga_journeys/models.py
+++ yoga_journeys/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from yoga_lessons.models import JourneyActiveYogaLesson
 from yoga_practices.models import JourneyCompletedYogaPractice, JourneyActiveYogaChallenge
 
 
@@ -11,6 +12,9 @@ class YogaJourney(models.Model):
     )
     active_yoga_challenges = models.ManyToManyField(
         "yoga_practices.YogaChallenge", through="yoga_practices.JourneyActiveYogaChallenge", blank=True
+    )
+    active_yoga_lessons = models.ManyToManyField(
+        "yoga_lessons.YogaLesson", through="yoga_lessons.JourneyActiveYogaLesson", blank=True
     )
 
     def complete_yoga_practice(self, yoga_practice_id):
@@ -27,3 +31,14 @@ class YogaJourney(models.Model):
             yoga_journey=self, yoga_challenge_id=yoga_challenge_id
         )
         active_yoga_challenge.completed_yoga_practices.add(yoga_practice_id)
+
+    def start_yoga_lesson(self, yoga_lesson_id):
+        JourneyActiveYogaLesson.objects.create(yoga_journey=self, yoga_lesson_id=yoga_lesson_id)
+
+    def complete_yoga_lesson_step(self, yoga_lesson_id, yoga_lesson_step_id):
+        active_yoga_lesson = JourneyActiveYogaLesson.objects.get(yoga_journey=self, yoga_lesson_id=yoga_lesson_id)
+        active_yoga_lesson.completed_lesson_steps.add(yoga_lesson_step_id)
+
+    def restart_yoga_lesson(self, yoga_lesson_id):
+        active_yoga_lesson = JourneyActiveYogaLesson.objects.get(yoga_journey=self, yoga_lesson_id=yoga_lesson_id)
+        active_yoga_lesson.completed_lesson_steps.all().delete()
